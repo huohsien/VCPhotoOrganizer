033d7741659a08ed1ad48516bf00259d63b2511f
Victor Jiang <huohsien@me.com>
2022-10-16 11:07:35 +0800

    My Initial Commit. Not the one done by Xcode automatically. In this one, Info.plist was edited for privay opening photo library

--- VCPhotoOrganizer/ViewController.swift
+++ VCPhotoOrganizer/ViewController.swift
@@ -6,14 +6,45 @@
 //
 
 import UIKit
+import Photos
+import AVKit
 
 class ViewController: UIViewController {
 
+    var albums:[PHAssetCollection] = [PHAssetCollection]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+
+        let authStatus = PHPhotoLibrary.authorizationStatus()
+
+        if (authStatus != .authorized) {
+            print("no authorization for photo library")
+            PHPhotoLibrary.requestAuthorization { (authStatus) in
+                if (authStatus == .authorized) {
+                    print("photo library authorized")
+                } else {
+                    print("unknown error")
+                    return
+                }
+            }
+        }
+        while (authStatus != .authorized) {}
+        getAllAlbumCollections()
+        print("albums[0]=\(albums[0])")
     }
 
+    func getAllAlbumCollections() {
+        
+        let albumsOptions = PHFetchOptions()
+//        albumsOptions.predicate = NSPredicate(format: "estimatedAssetCount > 0")
+        albumsOptions.sortDescriptors = [NSSortDescriptor(key: "startDate", ascending: true)]
 
+        let fetchedResultAlbums = PHAssetCollection.fetchAssetCollections(with:.album, subtype: .any, options: albumsOptions)
+        var albums = [PHAssetCollection]()
+        fetchedResultAlbums.enumerateObjects { (collection, idx, stop) in
+            albums.append(collection)
+        }
+    }
 }
 
